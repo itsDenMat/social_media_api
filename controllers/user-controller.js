38bd4afa0bd461cef20a210edc98f56d39bc0b75
itsDenMat <dennismateo93@gmail.com>
2022-10-20 22:49:25 -0700

    Start user controller with get all users and get one user

---
+++ controllers/user-controller.js
@@ -0,0 +1,35 @@
+const { json } = require('express');
+const { User, Thought } = require('../models');
+
+const userController = {
+    getUsers(req,res) {
+        User.find()
+        .select('-__v')
+        .then((dbUserData) => {
+            res.json(dbUserData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+
+    getSingleUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+        .select('-__v')
+        .populate('friends')
+        .populate('thoughts')
+        .then((dbUserData) => {
+            if(!dbUserData) {
+                return res.status(404).json({ message: 'User ID is not valid!'});
+            }
+            res.json(dbUserData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+
+
+};
