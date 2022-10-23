a7355329453f2a9871eb4d56063c975ffeab328a
itsDenMat <dennismateo93@gmail.com>
2022-10-22 22:46:47 -0700

    Add thought routes api

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const { getThoughts, getOneThought, createThought, updateThought, deleteThought, addReaction, removeReaction } = require('../../controllers/thought-controller');
+
+router.route("/").get(getThoughts).post(createThought);
+
+router.route("/:thoughtId").get(getOneThought).put(updateThought).delete(deleteThought);
+
+router.route("/:thoughtId/reactions").post(addReaction);
+
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
+
+module.exports = router;
